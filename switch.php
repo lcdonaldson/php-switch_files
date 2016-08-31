afb39b63573bfbb951e4facc48e248b2389059a7
Levi Donaldson <lcdbacanas@gmail.com>
2016-08-30 22:24:15 -0500

    added switch

--- switch.php
+++ switch.php
@@ -21,5 +21,26 @@
                 echo "white";
         endswitch;
     ?>
+
+    /* the switch below is the same thing but it can be written slightly different. */
+
+    <?php
+        $a = 9;
+        
+        switch($a) {
+
+            case 1:
+                echo "blue";
+                break;
+            case 2:
+                echo "green";
+                break;
+            case 3:
+                echo "red";
+                break;
+            default:
+                echo "white";
+        };
+    ?>
     </body>
 </html>
